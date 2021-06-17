e89f04daf527348dbccf51a914cdebf765f95de2
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-06-17 16:37:09 -0500

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -1,27 +1,27 @@
-Write-Host 'Old way:'
-git diff HEAD HEAD~ --name-only
+# Write-Host 'Old way:'
+# git diff HEAD HEAD~ --name-only
 
 Write-Host 'New Way'
 git log -1 --name-only --oneline
 
-$editedFiles = @( git diff HEAD HEAD~ --name-only )
-Write-Host 'Edited files:'
-Write-Host $editedFiles
+# $editedFiles = @( git diff HEAD HEAD~ --name-only )
+# Write-Host 'Edited files:'
+# Write-Host $editedFiles
 
-$editedFilesFiltered = $editedFiles -match "rg/*"
+# $editedFilesFiltered = $editedFiles -match "rg/*"
 
-$directoriesWithChanges = `
-$(foreach ($file in $editedFilesFiltered) {
-    $file.split('/')[1]
-}) | Get-Unique
+# $directoriesWithChanges = `
+# $(foreach ($file in $editedFilesFiltered) {
+#     $file.split('/')[1]
+# }) | Get-Unique
 
-Write-Host 'RGs to deploy:'
-Write-Host $directoriesWithChanges
+# Write-Host 'RGs to deploy:'
+# Write-Host $directoriesWithChanges
 
-foreach ($directory in $directoriesWithChanges) {
-    $dirToCheck = './rg/' + $directory
-    # Get-ChildItem -Path $dirToCheck
-    $commandToExecute = $dirToCheck + '/deploy.ps1'
-    Invoke-Command -FilePath $commandToExecute
-}
+# foreach ($directory in $directoriesWithChanges) {
+#     $dirToCheck = './rg/' + $directory
+#     # Get-ChildItem -Path $dirToCheck
+#     $commandToExecute = $dirToCheck + '/deploy.ps1'
+#     Invoke-Command -FilePath $commandToExecute
+# }
 
